b1bfbb8636916b7c08a77b81f0f365896f68bc3a
Sameer Kurhade <samkurhade181@gmail.com>
2022-07-23 18:14:26 +0530

    Login system completed

---
+++ src/main/java/com/smart/entities/User.java
@@ -0,0 +1,117 @@
+package com.smart.entities;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name = "USER")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@NotBlank(message = "Name field is required!!")
+	@Size(min = 2,max = 20 , message = "minimum 2 and maximum 20 characters are allowed")
+	private String name;
+	@Column(unique = true)
+	@Pattern(regexp="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$",message = "Invalid email")
+	private String email;
+	@NotBlank(message = "Password field is required!!")
+	//changed the maximum size because getting error when added BCryptPasseordEncoder
+	@Size(min = 5,max = 100 , message = "minimum 5 and maximum 20 characters are allowed")
+	private String password;
+	private String role;
+	private String imageUrl;
+	@Column(length = 500)
+	private String about;
+	private boolean enabled;
+	
+	@OneToMany(cascade = CascadeType.ALL ,fetch = FetchType.LAZY,mappedBy = "user")
+	private List<Contact> contacts = new ArrayList<>();
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role
+				+ ", imageUrl=" + imageUrl + ", about=" + about + ", enabled=" + enabled + ", contacts=" + contacts
+				+ "]";
+	}
+	
+	
+	
+}
